c8b142287b075315f920e8738246e78c20fb09ce
ukasha36 <ukashashabbir36@gmail.com>
2024-03-27 12:37:55 +0500

    Context and Fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,73 @@
 // import List from './Components/List';
-import { useEffect, useState } from 'react';
-import './App.css';
-import Counter from './Components/Counter.jsx';
-import Child from './Components/Child.jsx';
+import React, { useEffect, useState, useRef } from "react";
+import "./App.css";
+import Counter from "./Components/Counter.jsx";
+import Child from "./Components/Child.jsx";
 
 // import { useState } from 'react';
 // import  Siderbar from  "./Components/Siderbar.jsx";
 // import Dashboard from "./Components/Dashboard.jsx"
 
 function App() {
-// const [name , setname] = useState("ukasha") ; 
-// const [value , setvalue] = useState("")
-//   const products = [
-//     { title: 'Cabbage', isFruit: false, id: 1 },
-//     { title: 'Garlic', isFruit: false, id: 2 },
-//     { title: 'Apple', isFruit: true, id: 3 },
-//     { title: 'banana', isFruit: true, id: 4 },
-//     { title: 'Carrot', isFruit: false, id: 4 },
-//   ];
-const [value , setvalue ] = useState(1)
-const [name , setname ] = useState("")
-const [count , setcount] = useState(0);
+  // const [name , setname] = useState("ukasha") ;
+  // const [value , setvalue] = useState("")
+  //   const products = [
+  //     { title: 'Cabbage', isFruit: false, id: 1 },
+  //     { title: 'Garlic', isFruit: false, id: 2 },
+  //     { title: 'Apple', isFruit: true, id: 3 },
+  //     { title: 'banana', isFruit: true, id: 4 },
+  //     { title: 'Carrot', isFruit: false, id: 4 },
+  //   ];
+  const [value, setvalue] = useState(1);
+  const [name, setname] = useState("");
+  const [count, setcount] = useState(0);
+  const [task, settask] = useState([]);
 
-useEffect(()=>{
-console.log("testing1..")
-},[])
+  const taskRef = useRef(null);
 
-useEffect(()=>{
-console.log("testing2 ..")
-})
+  // useEffect(()=>{
+  // console.log("testing1..")
+  // },[])
 
-useEffect(()=>{
-  console.log("testing three..")
-} , [name])
+  // useEffect(()=>{
+  // console.log("testing2 ..")
+  // })
 
+  // useEffect(()=>{
+  //   console.log("testing three..")
+  // } , [name])
 
-
+  const handleSubmit = () => {
+    const { current } = taskRef
+    settask([ current.value, ...task])
+    console.log(taskRef)
+  };
 
   return (
     <>
+      <Child count={count} />
+      <button
+        onClick={() => {
+          setcount(count + 1);
+        }}
+      >
+        Child Inc 
+      </button>
+
+      <div>
+        {/* onChange={(e)=>settask(e.target.value) } */}
+
+        <input type="text" ref={taskRef} placeholder="enter task " />
+        <button onClick={handleSubmit}> Add </button>
+
+      {<ul>
+        {
+          task.map((el, idx) => <li key={idx}>{el}</li>)
+        }
+      </ul>
+        } 
+      </div>
+
       {/* <h1   style={{color : "brown"}}>My {name } List  </h1>
 
       <button onClick={() => setname( "Muhammad Ukasha")}> Change the Value </button>
@@ -50,31 +79,39 @@ useEffect(()=>{
      <h2> {value}</h2>
      <button onClick={() => setvalue("")}>Reset Value</button> */}
 
-     
       {/* <Dashboard/> */}
 
       <div className="">
         <h1> Name : {name} </h1>
-        <button onClick={()=>{ setname("M Ukasha")}}> Change  </button>
-        
-         
+        <button
+          onClick={() => {
+            setname("M Ukasha");
+          }}
+        >
+          {" "}
+          Change{" "}
+        </button>
       </div>
-      
-     
 
       <div className="">
-     { value < 20 && <Counter value={value} /> 
-     
-     }
-        <button onClick={()=>{ setvalue(value + 1)}}> Inc </button>
-        <button onClick={()=>{ setvalue(value - 1)}}> Dec </button>
-         
+        {value < 20 && <Counter value={value} />}
+        <button
+          onClick={() => {
+            setvalue(value + 1);
+          }}
+        >
+          {" "}
+          Inc{" "}
+        </button>
+        <button
+          onClick={() => {
+            setvalue(value - 1);
+          }}
+        >
+          {" "}
+          Dec{" "}
+        </button>
       </div>
-
-      
-       <Child count={count} />
-        <button onClick={()=>{ setcount(count + 1)}}>Child  Inc </button>
-
     </>
   );
 }
--- src/Components/Child.jsx
+++ src/Components/Child.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+ 
 import { memo } from 'react';
 
 const Child = ({count}) => {
